452656f9a5efcbbf90186aec78435431175c760d
Markteisha Gill <markteishagill@Markteishas-MacBook-Pro.local>
2022-03-22 16:19:00 -0400

    completed pong

--- main.js
+++ main.js
@@ -1,30 +1,111 @@
-// Size of the game area (in px)
 const GAME_AREA_WIDTH = 700;
+
 const GAME_AREA_HEIGHT = 500;
 
-// Size of the paddles (in px)
 const PADDLE_HEIGHT = 100;
+
 const PADDLE_WIDTH = 20;
 
-// Size of the ball (in px)
 const BALL_SIZE = 20;
 
-// Get the computer paddle element
-const computerPaddle = document.querySelector('.computer-paddle');
 
-// The y-velocity of the computer paddle
+
+
+const computerPaddle = document.querySelector(".computer-paddle");
+
+const playerPaddle = document.querySelector(".player-paddle");
+
+const ball = document.querySelector(".ball");
+
+//Velocity - how the ball moves over time
+
 let computerPaddleYPosition = 0;
-let computerPaddleYVelocity = 1;
 
-// Update the pong world
+let computerPaddleYVelocity = 5;
+
+//Createds movement of the paddles. P.S. never go over 15 for velocity because it is too fast!
+
+let playerPaddleYPosition = 100;
+
+let playerPaddleYVelocity = 5;
+
+
+// Above the update() funciton, create variables to store the ball's x-position, y-position, x-velocity, and y-velocity
+let ballYPosition = 250;
+let ballXPosition = 350;
+
+let ballYVelocity = 5;
+let ballXVelocity = 5;
+
+
+
+
 function update() {
 
-    // Update the computer paddle's position
-    computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
+  computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
 
-    // Apply the y-position 
-    computerPaddle.style.top = `${computerPaddleYPosition}px`;
+  playerPaddleYPosition = playerPaddleYPosition + playerPaddleYVelocity;
+
+
+//Call Computer paddle position
+  computerPaddle.style.top = `${computerPaddleYPosition}px`;
+
+  if (computerPaddleYPosition <= 0 || computerPaddleYPosition >= 400) {
+
+    computerPaddleYVelocity = computerPaddleYVelocity * -1;
+
+  }
+
+
+
+//Call Player Paddle position
+  playerPaddle.style.top = `${playerPaddleYPosition}px`;
+
+  if (playerPaddleYPosition <= 0 || playerPaddleYPosition >= 400) {
+
+    playerPaddleYVelocity = playerPaddleYVelocity * -1;
+
+  }
+
+
+
+//This keeps the ball in Motion.
+  ballYPosition = ballYPosition + ballYVelocity;
+
+  ballXPosition = ballXPosition + ballXVelocity;
+
+  ball.style.left = ballXPosition + "px";
+
+  ball.style.top = ballYPosition + "px";
+
+
+
+
+  if (ballXPosition > 680) {
+
+    ballXVelocity = ballXVelocity * -1;
+
+  }
+
+  if (ballXPosition < 1) {
+
+    ballXVelocity = ballXVelocity * -1;
+
+  }
+
+  if (ballYPosition > 480) {
+
+    ballYVelocity = ballYVelocity * -1;
+
+  }
+
+  if (ballYPosition < 1) {
+
+    ballYVelocity = ballYVelocity * -1;
+
+  }
+
 }
 
-// Call the update() function every 35ms
+
 setInterval(update, 35);
